65eac050028d235a4a44684e365ebe1031eedc3c
KJ Schneider <kjdschneider@gmail.com>
2019-10-08 00:07:53 -0400

    I definitely had too much fun with the stretch goal, lol

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,33 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject (gameAttrs) {
+  this.createdAt = gameAttrs.createdAt;
+  this.name = gameAttrs.name;
+  this.dimensions = gameAttrs.dimensions;
+}
+
+GameObject.prototype.destroy = function (){
+  return `${this.name} was removed from the game!!!`;
+}
+
 /*
   === CharacterStats ===
-  * healthPoints
+  * healthPointsPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats (charAttrs){
+  GameObject.call(this, charAttrs);
+  this.healthPointsPoints = charAttrs.healthPointsPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage!`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +53,22 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+function Humanoid (humanAttrs){
+  CharacterStats.call(this, humanAttrs);
+  this.team = humanAttrs.team;
+  this.weapons = humanAttrs.weapons;
+  this.language = humanAttrs.language;
+  this.healthPoints = humanAttrs.healthPoints;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -39,10 +76,74 @@
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+
+// STRETCH STUFF - Hero and Villain Constructors
+function Hero (heroAttrs){
+  Humanoid.call(this, heroAttrs);
+  this.heroCatchPhrase = heroAttrs.heroCatchPhrase;
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+Hero.prototype.herosSmile = function(){
+  this.healthPoints = this.healthPoints - Math.round(Math.random() * 10);
+  console.log(this.takeDamage());
+  console.log(`${this.name} hp = ${this.healthPoints}`);
+  if (this.healthPoints <= 0){
+    console.log(this.destroy());
+  }
+}
+
+Hero.prototype.plotArmor = function(){
+  this.healthPoints = Math.round(this.healthPoints + Math.round(Math.random() * 15));
+  console.log(`${this.name}, I need you!`);
+  console.log(`${this.name} slowly stands to their feet, fire raging within their heart. Their health has gone up a bit.`);
+  console.log(`${this.name} hp = ${this.healthPoints}`);
+}
+
+Hero.prototype.powerOfFriendship = function(){
+  this.healthPoints = 0;
+  console.log(`In display of unbelievable bright, blinding light, the power of friendship overwhelms ${finalBoss.name}. He collapses to the ground, his form smoking, as his evil body falls to ashes.`);
+  this.destroy();
+}
+
+
+function Villain (villainAttrs){
+  Humanoid.call(this, villainAttrs);
+  this.villainCatchPhrase = villainAttrs.villainCatchPhrase;
+}
+
+Villain.prototype = Object.create(Humanoid.prototype);
+
+Villain.prototype.villainsMonologue = function(){
+  this.healthPoints = 1;
+  console.log(`${this.name}, prepare to die! For I, the evil Lord of all that is evil and bad, shall destroy you once and for all here on this very soil! You could never hope to.... *the villain rambles on, somewhat coherently, for approximately 14 and a half minutes*`);
+  console.log(this.takeDamage());
+  console.log(`${this.name} hp = ${this.healthPoints}`);
+  if (this.healthPoints <= 0){
+    console.log(this.destroy());
+  }
+}
+
+Villain.prototype.secondPhase = function(){
+  this.healthPoints = this.healthPoints + Math.round(Math.random() * 150);
+  console.log(`This isn't even my final form!!!`);
+  console.log(`${finalBoss.name} recieves a ton of health as he transforms into the epitomy of true evil!`)
+  console.log(`${this.name} hp = ${this.healthPoints}`);
+}
+
+Villain.prototype.annihilation = function(){
+  this.healthPoints = 0.00000001;
+  console.log(this.takeDamage());
+  console.log(`${this.name} falls to their knees. "Urgh. It can't end like this."`);
+  console.log(`${this.name} hp = ${this.healthPoints}`);
+}
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
+
+  const mage = new Hero({
+    heroCatchPhrase: 'True power... *makes eye contact* is from within!',
     createdAt: new Date(),
     dimensions: {
       length: 2,
@@ -58,7 +159,8 @@
     language: 'Common Tongue',
   });
 
-  const swordsman = new Humanoid({
+  const swordsman = new Hero({
+    heroCatchPhrase: 'Yaaaaaaaaaaaa!',
     createdAt: new Date(),
     dimensions: {
       length: 2,
@@ -75,7 +177,8 @@
     language: 'Common Tongue',
   });
 
-  const archer = new Humanoid({
+  const archer = new Hero({
+    heroCatchPhrase: 'Take this arrow to the knee, fiend!',
     createdAt: new Date(),
     dimensions: {
       length: 1,
@@ -92,19 +195,100 @@
     language: 'Elvish',
   });
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+  // STRETCH VILLAIN OVERLORD
+  const finalBoss = new Villain({
+    villainCatchPhrase: 'The power of friendship shall shatter beneath my inescapable will! *maniacal laughing*',
+    createdAt: new Date(),
+    dimensions: {
+      length: 3,
+      width: 2,
+      height: 7
+    },
+    healthPoints: 25,
+    name: 'Kane of Enupnion',
+    team: 'Bad guys',
+    weapons: [
+      'Infinity Gauntlet',
+      'Surprising Revelation about the Hero'
+    ],
+    language: 'Pure evil'
+  })
+
+  const heroArray = [mage, archer, swordsman];
+  const firstHero = heroArray[Math.floor(Math.random() * 3)];
+  const heroArray2 = heroArray.filter( item => item !== firstHero);
+  const secondHero = heroArray2[Math.floor(Math.random() * 2)];
+  const finalHeroArr = heroArray2.filter( item => item !== secondHero);
+  const finalHero = finalHeroArr[0];
+  // -------------MVP checks----------------
+  // console.log(mage.createdAt); // Today's date
+  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  // console.log(swordsman.healthPoints); // 15
+  // console.log(mage.name); // Bruce
+  // console.log(swordsman.team); // The Round Table
+  // console.log(mage.weapons); // Staff of Shamalama
+  // console.log(archer.language); // Elvish
+  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  // console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Give the Hero and Villains different methods that could be used to remove healthPoints points from objects which could result in destruction if healthPoints gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  // ------------STRETCH Battle---------
+  console.log('On a smoky battlefield..... the forces of good and evil approach each other.');
+  console.log('');
+  console.log(`The mage, called ${mage.name}, steps forward. "${mage.heroCatchPhrase}"`);
+  console.log('');
+  console.log(`${archer.name} the archer takes aim, notching her arrow. "${archer.heroCatchPhrase}"`);
+  console.log('');
+  console.log(`The dark Lord of evil and murdered puppies, ${finalBoss.name}, stands to his feet, a smile creeping across his features. "${finalBoss.villainCatchPhrase}"`);
+  console.log('');
+  console.log(`Finally, ${swordsman.name} leaps in front of the band of heros, sword in hand, yelling his warcry as he charges their mortal enemy. "${swordsman.heroCatchPhrase}"`);
+  console.log('');
+  console.log(`${swordsman.name} lunges forward, swinging his ${swordsman.weapons[0]}. He flashes a smile.`);
+  swordsman.herosSmile.call(finalBoss);
+  console.log('');
+  console.log(`${archer.name} fires a volley of arrows with her ${archer.weapons[0]}. Two land as direct hits! Her snow white teeth gleam in the burning sunlight.`);
+  archer.herosSmile.call(finalBoss);
+  archer.herosSmile.call(finalBoss);
+  console.log('');
+  console.log(`The villain staggers back, his face a grimace. ${mage.name} flashes one last heoric smile as a spell of great power erupts from his ${mage.weapons[0]}. The sight of his flawless teeth terrifies the ruffian.`);
+  mage.herosSmile.call(finalBoss);
+  console.log('');
+  console.log('..... the dust slowly settles from the heroes\' barrage.....')
+  console.log('');
+  if (finalBoss.healthPoints <= 0){
+    console.log('Our heroes have prevailed, due to the power of frienship! Because of course they have!')
+  } else if (finalBoss.healthPoints > 0){
+    console.log(`${finalBoss.name} rises to his feet, a vicious snicker creeping up his evil throat.`);
+    finalBoss.secondPhase();
+    console.log('');
+    console.log(`${finalBoss.name} strikes back, aiming his loathing power at ${firstHero.name} and ${secondHero.name}.`);
+    console.log('');
+    finalBoss.annihilation.call(firstHero);
+    finalBoss.annihilation.call(secondHero);
+    console.log('');
+    console.log('The villain turns his head to the final remaining hero.');
+    console.log('...');
+    console.log('');
+    finalBoss.villainsMonologue.call(finalHero);
+    console.log('');
+    console.log('');
+    console.log('');
+    console.log(`${finalHero.name} struggles to remain on their feet. Darkness claws at the edges of their vision. They pull their ${finalHero.weapons[finalHero.weapons.length -1]} out, ready for the final showdown.`);
+    console.log('');
+    finalHero.plotArmor.call(firstHero);
+    console.log('');
+    finalHero.plotArmor.call(secondHero);
+    console.log('');
+    console.log('Our band of heroes stands together, preparing for one final attack.');
+    console.log('');
+    finalHero.powerOfFriendship.call(finalBoss);
+    console.log('');
+    console.log('Our heroes have prevailed, due to the power of frienship! Because of course they have!');
+  }
+  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global binding - when 'this' is declared on the global level. It references the 'window' or the 'console', essentially referring to what it is that makes up JS
+* 2. Implicit binding - when used within another context in an object (a function for example). If using dot notation, it always refers to the object to the left of the dot.
+* 3. New binding - when used with a constructor and the 'new' keyword, it refers to the new object that is created with the constructor.
+* 4. Explicit binding - when .bind, .call, or .apply is used to intentionally bind the 'this' keyword to some object in particular.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,51 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+const exObj = {
+    name: 'object',
+    type: 'example',
+    speak: function(){
+        return `${this.name} ${this.type}`;
+    }
+}
+
+console.log(exObj.speak());
 
 // Principle 3
 
 // code example for New Binding
+function Clothes (attrs){
+    this.type = attrs.type;
+    this.size = attrs.size;
+    this.fabric = attrs.fabric;    //each time the 'this' keyword is used here, it will refer to the new 
+}
+
+const shirt = new Clothes ({       //object that is created down here
+    type: 'Shirt',
+    size: 'Large',
+    fabric: 'Cotton'
+});
 
 // Principle 4
 
 // code example for Explicit Binding
+const obj1 = {
+    name: 'KJ',
+    birthYear: 1995
+}
+
+const obj2 = {
+    name: 'Bobby',
+    birthYear: 1863
+}
+
+function introduction (){
+    console.log(`Hi! My name is ${this.name} and I was born in the year ${this.birthYear}`);
+}
+
+introduction.call(obj1);
